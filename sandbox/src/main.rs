79267f4ce3b355e75cae581c01053fa28036d997
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2016-10-24 15:18:53 +0900

    refactor

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -3,70 +3,78 @@ extern crate advapi32;
 extern crate kernel32;
 
 use std::ffi::CString;
+use std::mem::size_of;
 use std::ptr::{null, null_mut};
 use winapi::LUID;
-use winapi::winnt::{self, TOKEN_ADJUST_PRIVILEGES, TOKEN_QUERY};
-use kernel32::GetCurrentProcess;
-use advapi32::{OpenProcessToken, LookupPrivilegeValueA};
+use winapi::winerror::ERROR_SUCCESS;
+use winapi::winnt;
+
+
+struct Handle(winnt::HANDLE);
+
+impl Handle {
+  fn new(h: winnt::HANDLE) -> Handle {
+    Handle(h)
+  }
+
+  fn as_raw(&self) -> winnt::HANDLE {
+    self.0
+  }
+}
+
+impl Drop for Handle {
+  fn drop(&mut self) {
+    unsafe { kernel32::CloseHandle(self.0) };
+    self.0 = null_mut();
+  }
+}
 
 
 fn enable_priviledges(name: &str, enable: bool) -> bool {
   // 1. retrieve the process token of current process.
   let mut h_token = null_mut();
   let ret = unsafe {
-    OpenProcessToken(GetCurrentProcess(),
-                     TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY,
+    advapi32::OpenProcessToken(kernel32::GetCurrentProcess(),
+                     winnt::TOKEN_ADJUST_PRIVILEGES | winnt::TOKEN_QUERY,
                      &mut h_token)
   };
   if ret == 0 {
-    println!("[debug] failed to retrieve process token");
     return false;
   }
+  let h_token = Handle::new(h_token);
 
   // 2. retrieve a LUID for given priviledge
-  let mut symlink_luid = LUID {
+  let mut luid = LUID {
     LowPart: 0,
     HighPart: 0,
   };
   let ret = unsafe {
     let name = CString::new(name).unwrap();
-    LookupPrivilegeValueA(null(), name.as_ptr(), &mut symlink_luid)
+    advapi32::LookupPrivilegeValueA(null(), name.as_ptr(), &mut luid)
   };
   if ret == 0 {
-    println!("[debug] failed to retrieve a LUID");
-    unsafe { kernel32::CloseHandle(h_token) };
     return false;
   }
 
-  let token_priviledges = vec![0u8; std::mem::size_of::<winnt::TOKEN_PRIVILEGES>() + 1];
+  let token_priviledges = vec![0u8; size_of::<winnt::TOKEN_PRIVILEGES>() + 1];
   let mut p = token_priviledges.as_ptr() as *mut winnt::TOKEN_PRIVILEGES;
   unsafe {
     (*p).PrivilegeCount = 1;
-    (*((*p).Privileges.as_ptr() as *mut winnt::LUID_AND_ATTRIBUTES)).Luid = symlink_luid;
-    (*((*p).Privileges.as_ptr() as *mut winnt::LUID_AND_ATTRIBUTES)).Attributes = if enable {
-      winnt::SE_PRIVILEGE_ENABLED
-    } else {
-      0
-    };
+    (*((*p).Privileges.as_ptr() as *mut winnt::LUID_AND_ATTRIBUTES)).Luid = luid;
+    (*((*p).Privileges.as_ptr() as *mut winnt::LUID_AND_ATTRIBUTES)).Attributes =
+      if enable { winnt::SE_PRIVILEGE_ENABLED } else { 0 };
   }
 
   unsafe {
-    advapi32::AdjustTokenPrivileges(h_token,
-                                    0,
-                                    token_priviledges.as_ptr() as *mut winnt::TOKEN_PRIVILEGES,
-                                    0,
-                                    null_mut(),
-                                    null_mut());
+    advapi32::AdjustTokenPrivileges(h_token.as_raw(),
+                          0,
+                          token_priviledges.as_ptr() as *mut winnt::TOKEN_PRIVILEGES,
+                          0,
+                          null_mut(),
+                          null_mut());
   }
 
-  if unsafe { kernel32::GetLastError() } == winapi::winerror::ERROR_SUCCESS {
-    unsafe { kernel32::CloseHandle(h_token) };
-    true
-  } else {
-    println!("[debug] failed to adjust priviledge");
-    unsafe { kernel32::CloseHandle(h_token) };
-    false
-  }
+  (unsafe { kernel32::GetLastError() }) == ERROR_SUCCESS
 }
 
 
