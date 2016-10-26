a08ef2237e53bc7b412986c64eaa90a8fbb0cbc7
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2016-10-27 00:29:48 +0900

    improve CI configurations

--- build.rs
+++ build.rs
@@ -1,10 +1,7 @@
 extern crate clap;
-extern crate rustc_cfg;
 
 #[path = "src/cli.rs"]
 mod cli;
-
-use rustc_cfg::Cfg;
 
 fn main() {
   let out_dir = std::path::Path::new(env!("CARGO_MANIFEST_DIR")).join("completions");
@@ -14,10 +11,4 @@ fn main() {
   cli::build_cli().gen_completions(pkg_name, clap::Shell::Bash, &out_dir);
   cli::build_cli().gen_completions(pkg_name, clap::Shell::Zsh, &out_dir);
   cli::build_cli().gen_completions(pkg_name, clap::Shell::Fish, &out_dir);
-
-  let cfg = Cfg::new(std::env::var_os("TARGET").unwrap()).unwrap();
-
-  use std::io::Write;
-  let mut host = std::fs::OpenOptions::new().create(true).write(true).open("host-triplet").unwrap();
-  host.write(format!("{}-{}", cfg.target_arch, cfg.target_os).as_bytes()).unwrap();
 }
--- ci_deploy.bat
+++
@@ -1,7 +0,0 @@
-@echo on
-
-set FNAME=%TARGET%-%HOST%
-
-mkdir %FNAME%
-copy target\%HOST%\release\%TARGET%.exe .\%FNAME%\%TARGET%.exe
-copy completions\* .\%FNAME%\
