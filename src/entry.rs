2433354590563bcd50cdd4f763c4bcf500252b3c
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2016-11-02 03:36:14 +0900

    fix behaviour of mklink()

--- src/entry.rs
+++ src/entry.rs
@@ -1,8 +1,8 @@
 use std::io;
+use std::fs;
 use std::path::{Path, PathBuf, MAIN_SEPARATOR};
 use ansi_term;
 use util;
-
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum EntryStatus {
@@ -64,12 +64,18 @@ impl Entry {
   }
 
   pub fn mklink(&self, dry_run: bool, verbose: bool) -> Result<(), io::Error> {
-    if self.status() == EntryStatus::Healthy {
-      if verbose {
-        println!("{}\n  the link has already existed.", self.dst.display());
-      }
-      return Ok(());
+    if !self.src.exists() || self.status() == EntryStatus::Healthy {
+      return Ok(()); // Do nothing.
     }
+
+    if self.dst.exists() && !try!(Self::is_symlink(&self.dst)) {
+      let origpath = Self::orig_path(&self.dst);
+      println!("file {} has already existed. It will be renamed to {}",
+               self.dst.display(),
+               origpath.display());
+      try!(fs::rename(&self.dst, origpath));
+    }
+
     if verbose {
       println!("{}\n  => {}", self.dst.display(), self.src.display());
     }
@@ -77,9 +83,30 @@ impl Entry {
   }
 
   pub fn unlink(&self, dry_run: bool, verbose: bool) -> Result<(), io::Error> {
+    if !self.dst.exists() || !try!(Self::is_symlink(&self.dst)) {
+      return Ok(()); // do nothing
+    }
+
     if verbose {
       println!("unlink {}", self.dst.display());
     }
-    util::remove_link(&self.dst, dry_run)
+    try!(util::remove_link(&self.dst, dry_run));
+
+    let origpath = Self::orig_path(&self.dst);
+    if origpath.exists() {
+      try!(fs::rename(origpath, &self.dst));
+    }
+
+    Ok(())
+  }
+
+  fn orig_path<P: AsRef<Path>>(path: P) -> PathBuf {
+    let origpath = format!("{}.bk", path.as_ref().to_str().unwrap());
+    Path::new(&origpath).to_path_buf()
+  }
+
+  fn is_symlink<P: AsRef<Path>>(path: P) -> Result<bool, io::Error> {
+    let meta = try!(path.as_ref().symlink_metadata());
+    Ok(meta.file_type().is_symlink())
   }
 }
