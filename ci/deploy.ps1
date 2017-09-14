2db6a0d0e9e56890b4040b0f3174b304b6f0c089
Yusuke Sasaki <y_sasaki@nuem.nagoya-u.ac.jp>
2017-09-14 14:42:52 +0900

    chore(ci): import template from `japaric/trust`

--- ci/deploy.ps1
+++
@@ -1,9 +0,0 @@
-param(
-  [parameter(mandatory)]
-  [string]$pkgname
-)
-
-Remove-Item -Recurse -Force ".\$($pkgname)" -ErrorAction SilentlyContinue
-cargo install --root ".\$($pkgname)"
-
-7z a "$($pkgname).zip" ".\$($pkgname)\"
--- ci/install.sh
+++ ci/install.sh
@@ -1,70 +1,31 @@
 #!/bin/bash
 
-script_dir="$(cd $(dirname $BASH_SOURCE); pwd)"
+# copied from trust
 
-set -euo pipefail
+set -ex
 
 main() {
-  case `uname -s` in
-    Linux)
-      # Install Rustup toolchain
-      curl -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain "$toolchain"
-
-      # Install target
-      default_target="`$HOME/.cargo/bin/rustup target list | grep default | awk '{print $1}'`"
-      if ! [[ "$target" = "$default_target" ]]; then
-        "$HOME/.cargo/bin/rustup" target add "$target"
-      fi
-
-      # Launch docker container for building
-      docker rm -f "$container_name" || true
-      docker run --name "$container_name" -d -it --privileged \
-        -v "$(pwd)":$HOME/src -w $HOME/src \
-        -v "$HOME/.cargo":$HOME/.cargo \
-        -v "$HOME/.rustup":$HOME/.rustup \
-        "$image_name"
-      docker exec -it "$container_name" useradd -ms /bin/bash "$USER"
-
-      if ! [[ "$target" = "$default_target" ]]; then
-        mkdir -p $script_dir/../.cargo
-        echo -e "[build]\ntarget = \"$target\"" | tee $script_dir/../.cargo/config
-        case $target in
-          arm-linux-androideabi|i686-linux-android)
-            echo -e "\n[target.$target]\nlinker = \"$target-gcc\"" | tee -a $script_dir/../.cargo/config ;;
-        esac
-      fi
-      ;;
-
-    Darwin)
-      curl -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain "$toolchain" --default-host "$target"
-      ;;
-  esac
-}
-
-#= PARAMETERS ===================================
-target="${1:-x86_64-unknown-linux-gnu}"
-toolchain="${2:-stable}"
-container_name="${3:-rust}"
-api="${API:-24}"
-
-case $target in
-  *arm-linux-androideabi*|*i686-linux-android*)
-    image_name="ubntintrepid/${target}:api${api}"
-    if [[ "$target" = "arm-linux-androideabi" ]]; then arch=arm; fi
-    if [[ "$target" = "i686-linux-android"    ]]; then arch=x86; fi
-    if [[ `docker images -q "$image_name" | wc -l` = 0 ]]; then
-      docker build -t "$image_name" \
-        --build-arg ARCH="$arch" \
-        --build-arg API="$api" \
-        "$script_dir/rust-android-builder"
+    local target=
+    if [ $TRAVIS_OS_NAME = linux ]; then
+        target=x86_64-unknown-linux-musl
+        sort=sort
+    else
+        target=x86_64-apple-darwin
+        sort=gsort  # for `sort --sort-version`, from brew's coreutils.
     fi
-    ;;
-  *-apple-darwin)
-    ;;
-  *)
-    image_name="japaric/${target}:latest"
-    docker pull "$image_name"
-    ;;
-esac
+
+    # This fetches latest stable release
+    local tag=$(git ls-remote --tags --refs --exit-code https://github.com/japaric/cross \
+                       | cut -d/ -f3 \
+                       | grep -E '^v[0.1.0-9.]+$' \
+                       | $sort --version-sort \
+                       | tail -n1)
+    curl -LSfs https://japaric.github.io/trust/install.sh | \
+        sh -s -- \
+           --force \
+           --git japaric/cross \
+           --tag $tag \
+           --target $target
+}
 
 main
